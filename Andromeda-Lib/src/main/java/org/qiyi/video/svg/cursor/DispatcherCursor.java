29ddfb6108f1dbed28671763b8667eae112245f8
allenwang <bettarwang@gmail.com>
2018-05-29 20:23:03 +0800

    fixed cursor-cache bug and upgrade core lib version to 1.1.2

--- Andromeda-Lib/src/main/java/org/qiyi/video/svg/cursor/DispatcherCursor.java
+++ Andromeda-Lib/src/main/java/org/qiyi/video/svg/cursor/DispatcherCursor.java
@@ -1,27 +1,27 @@
 /*
-* Copyright (c) 2018-present, iQIYI, Inc. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*
-*        1. Redistributions of source code must retain the above copyright notice,
-*        this list of conditions and the following disclaimer.
-*
-*        2. Redistributions in binary form must reproduce the above copyright notice,
-*        this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-*
-*        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
-*        from this software without specific prior written permission.
-*
-*        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-*        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-*        IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
-*        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-*        OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-*        OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-*        EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*
-*/
+ * Copyright (c) 2018-present, iQIYI, Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *        1. Redistributions of source code must retain the above copyright notice,
+ *        this list of conditions and the following disclaimer.
+ *
+ *        2. Redistributions in binary form must reproduce the above copyright notice,
+ *        this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ *        3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived
+ *        from this software without specific prior written permission.
+ *
+ *        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *        INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *        IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ *        OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ *        OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *        OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *        EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 package org.qiyi.video.svg.cursor;
 
 import android.database.Cursor;
@@ -43,7 +43,7 @@ public class DispatcherCursor extends MatrixCursor {
 
     public static final String KEY_BINDER_WRAPPER = "KeyBinderWrapper";
 
-    private static Map<String, DispatcherCursor> cursorMap = new ConcurrentHashMap<>();
+    private static Map<String, DispatcherCursor> cursorCache = new ConcurrentHashMap<>();
 
     public static final String[] DEFAULT_COLUMNS = {"col"};
 
@@ -61,9 +61,13 @@ public class DispatcherCursor extends MatrixCursor {
 
     public static DispatcherCursor generateCursor(IBinder binder) {
         try {
-            DispatcherCursor cursor = cursorMap.get(binder.getInterfaceDescriptor());
+            DispatcherCursor cursor;
+            cursor = cursorCache.get(binder.getInterfaceDescriptor());
+            if (cursor != null) {
+                return cursor;
+            }
             cursor = new DispatcherCursor(DEFAULT_COLUMNS, binder);
-            cursorMap.put(binder.getInterfaceDescriptor(), cursor);
+            cursorCache.put(binder.getInterfaceDescriptor(), cursor);
             return cursor;
         } catch (RemoteException ex) {
             return null;
