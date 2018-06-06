2df12b9cea7f2af481b89f7ae8e61895ff9e7c12
Guo,Feng(RIM) <guofeng05@baidu.com>
2018-06-06 10:26:23 +0800

    fix toast bug and eventbus unregister

---
+++ app/src/main/java/wang/imallen/blog/servicemanager/utils/ToastUtil.java
@@ -0,0 +1,24 @@
+package wang.imallen.blog.servicemanager.utils;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * Created by guofeng05 on 2018/6/5.
+ */
+
+public class ToastUtil {
+    private static Handler mainHandler = new Handler(Looper.getMainLooper());
+
+    public static void toast(final Context context, final String str) {
+        mainHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
